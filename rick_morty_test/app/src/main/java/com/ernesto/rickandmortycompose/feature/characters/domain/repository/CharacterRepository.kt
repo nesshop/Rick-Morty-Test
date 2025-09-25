286f97454fec89dfac30c593920aa811dce9f65a
Ernesto <ernesst.developer@gmail.com>
2025-09-25 09:37:14 +0200

    🧪 Add characterRepositoryImpl test

--- rick_morty_test/app/src/main/java/com/ernesto/rickandmortycompose/feature/characters/domain/repository/CharacterRepository.kt
+++ rick_morty_test/app/src/main/java/com/ernesto/rickandmortycompose/feature/characters/domain/repository/CharacterRepository.kt
@@ -1,7 +1,6 @@
 package com.ernesto.rickandmortycompose.feature.characters.domain.repository
 
 import androidx.paging.PagingData
-import com.ernesto.rickandmortycompose.feature.characters.data.remote.dto.response.CharacterResponse
 import com.ernesto.rickandmortycompose.feature.characters.domain.model.CharacterModel
 import kotlinx.coroutines.flow.Flow
 
---
+++ rick_morty_test/app/src/test/java/com/ernesto/rickandmortycompose/feature/characters/data/repository/CharacterRepositoryImplTest.kt
@@ -0,0 +1,67 @@
+package com.ernesto.rickandmortycompose.feature.characters.data.repository
+
+import androidx.paging.PagingData
+import app.cash.turbine.test
+import com.ernesto.rickandmortycompose.feature.characters.data.local.CharactersLocalDataSource
+import com.ernesto.rickandmortycompose.feature.characters.data.remote.CharactersRemoteDataSource
+import com.ernesto.rickandmortycompose.feature.characters.data.remote.dto.response.CharacterResponse
+import com.ernesto.rickandmortycompose.feature.characters.data.remote.dto.response.CharactersDataResponse
+import com.ernesto.rickandmortycompose.feature.characters.data.remote.dto.response.InfoResponse
+import com.ernesto.rickandmortycompose.feature.characters.domain.model.CharacterModel
+import com.ernesto.rickandmortycompose.feature.characters.domain.repository.CharacterRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeout
+import org.junit.Assert.*
+import org.junit.Test
+
+class CharacterRepositoryImplTest {
+
+    private val remoteDataSource: CharactersRemoteDataSource = mockk()
+    private val localDataSource: CharactersLocalDataSource = mockk()
+    private val repository = CharacterRepositoryImpl(remoteDataSource, localDataSource)
+
+    @Test
+    fun `WHEN getAllCharacters called THEN returns Flow`() = runTest {
+        //WHEN
+        val result = repository.getAllCharacters()
+
+        //THEN
+        assertNotNull(result)
+        assertTrue(result is Flow<PagingData<CharacterModel>>)
+        assertTrue(result is Flow<*>)
+    }
+
+    @Test
+    fun `WHEN getAllCharacters called THEN uses correct page size`() = runTest {
+        //THEN
+        assertEquals(20, CharacterRepositoryImpl.MAX_ITEMS)
+    }
+
+    @Test
+    fun `WHEN getAllCharacters called multiple times THEN creates different Flow instances`() = runTest {
+        //WHEN
+        val result1 = repository.getAllCharacters()
+        val result2 = repository.getAllCharacters()
+
+        //THEN
+        assertNotEquals(result1, result2)
+        assertTrue(result1 is Flow<PagingData<CharacterModel>>)
+        assertTrue(result2 is Flow<PagingData<CharacterModel>>)
+    }
+
+    @Test
+    fun `WHEN repository is created THEN implements CharacterRepository interface`() {
+        // THEN
+        assertTrue("Repository should implement CharacterRepository", repository is CharacterRepository)
+    }
+
+    @Test
+    fun `WHEN repository is created THEN is properly initialized`() {
+        // THEN
+        assertNotNull("Repository should be properly initialized", repository)
+    }
+}
